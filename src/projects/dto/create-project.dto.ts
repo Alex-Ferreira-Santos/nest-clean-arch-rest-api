bbf7ee7280c38972e538afcf59c86f1dd2ebedc8
Alex Ferreira Santos <alexfstos@gmail.com>
2024-01-10 20:47:12 -0300

    feat: add project entity and creating project

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,7 @@
-export class CreateProjectDto {}
+export class CreateProjectDto {
+  name: string;
+  description: string;
+  started_at: Date | null;
+  cancelled_at: Date | null;
+  forecasted_at: Date | null;
+}
